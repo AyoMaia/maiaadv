325d60ad7af825535a7d8fef179a217f110682c5
AyoMaia <diogo.maia.pereira00@gmail.com>
2024-07-08 16:33:16 -0300

    maiaadvweb

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <section className="flex items-center justify-center h-full w-full bg-black">
+    <h1 className="text-white font-bold text-4xl">Maia Advocacia</h1>
+   </section>
+  );
+}
