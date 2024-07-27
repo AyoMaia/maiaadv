14b021bd220ea842d9b607f3dc9e958bab8fa927
AyoMaia <diogo.maia.pereira00@gmail.com>
2024-07-26 21:01:05 -0300

    ico

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,15 +14,14 @@ export function Hero() {
   return (
     <Container className="pb-36 pt-10 text-center lg:pt-12">
       
-      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-gray-100 sm:text-7xl">
-        Accounting for small businesses.
+      <h1 className="mx-auto max-w-4xl font-display text-2xl font-medium tracking-tight text-gray-100 sm:text-6xl">
+        Defendendo Seus Interesses Tributários com Conhecimento e Compromisso.
       </h1>
-      <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-gray-300">
-        Most bookkeeping software is accurate, but hard to use. We make the
-        opposite trade-off, and hope you don’t get audited.
+      <p className="mx-auto mt-6 max-w-2xl text-lg tracking-tight text-gray-300 font-semibold">
+        Defendendo seus interesses tributários com a mais alta competência e atenção
       </p>
       <div className="mt-10 flex justify-center gap-x-6">
-        <Button href="/register" className='text-xl'>Get 6 months free</Button>
+        <Button href="/register" className='text-xl'>Solicitar Consultoria </Button>
       </div>
       
     </Container>
